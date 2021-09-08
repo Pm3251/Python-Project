4fbffacdefcd6cb04dae7c434191fda1e986526c
Pm3251 <90075441+Pm3251@users.noreply.github.com>
2021-09-07 20:10:33 -0400

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,29 @@
+def welcome_assignment_answers(question):
+    if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
+        answer = "mTLS"
+   return (answer)
+    if  question == "Are encoding and encryption the same? - Yes/No":
+       answer = "No"
+   return (answer)
+    if question == "Is it possible to decrypt a message without a key? - Yes/No":
+       answer = "Yes"
+   return (answer)
+    if question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "No"
+   return (answer)
+    if question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "Yes"
+   return (answer)
+    if question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+        answer = "42B76FE51778764973077A5A94056724"
+   return (answer)
+   if question ==  "Is MD5 a secured hashing algorithm? - Yes/No"
+    answer = "No"
+   return (answer)
+    if question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer = int(5)
+   return (answer)
+    if question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        answer = int(4)
+    return (answer)
+
